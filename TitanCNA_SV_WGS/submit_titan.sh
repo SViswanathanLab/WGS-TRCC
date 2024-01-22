c88bda607d1cb9f86c23b8984a88ce425e255d2d
Mingkee Achom <40757619+miachom@users.noreply.github.com>
2024-01-22 10:49:17 -0500

    Update submit_titan.sh

--- TitanCNA_SV_WGS/submit_titan.sh
+++ TitanCNA_SV_WGS/submit_titan.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#module load use.own
-#source /mnt/storage/apps/anaconda3/etc/profile.d/conda.sh
-#conda activate base
-
-#module load /mnt/storage/spack/share/spack/modules/linux-ubuntu16.04-x86_64/samtools-1.9-gcc-5.4.0-jjq5nua
-#module load r-3.6.1-gcc-5.4.0-xtlkk5k
-
 cd /home/ma1111/tools/TitanCNA_SV_WGS
 
-#/mnt/storage/apps/miniconda3/bin/snakemake 
-
 /mnt/storage/apps/miniconda3/bin/snakemake -s /home/ma1111/tools/TitanCNA_SV_WGS/TitanCNA.snakefile --cluster-config /home/ma1111/tools/TitanCNA_SV_WGS/config/cluster_qsub.yaml --cluster "qsub -l h_vmem={cluster.h_vmem},h_rt={cluster.h_rt} -pe {cluster.pe} -binding {cluster.binding}" --jobs 30 --verbose --rerun-incomplete
-
-#/mnt/storage/apps/anaconda3-2021.05/bin/snakemake
-#
-#changyu /mnt/storage/apps/miniconda3/bin/snakemake
